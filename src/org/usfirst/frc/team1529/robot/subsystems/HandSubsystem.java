253b984cd03e1068a8319f0f278539a226cfc915
CyberCards <frc1529@gmail.com>
2018-02-03 16:49:16 -0500

    Opps

--- src/org/usfirst/frc/team1529/robot/subsystems/HandSubsystem.java
+++ src/org/usfirst/frc/team1529/robot/subsystems/HandSubsystem.java
@@ -1,18 +1,47 @@
 package org.usfirst.frc.team1529.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
 public class HandSubsystem extends Subsystem {
-
+	VictorSPX LeftIntake = new VictorSPX(7);
+	VictorSPX RightIntake = new VictorSPX(8);
+	VictorSPX HandRotate = new VictorSPX(9);
+	//actuators
+	
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
     public void initDefaultCommand() {
+    	
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public void OpenGrip(){
+    	//actuators
+    }
+    public void CloseGrip(){
+    	//actuators
+    }
+    public void Intake(){
+    	LeftIntake.set(ControlMode.PercentOutput, 75);
+    	RightIntake.set(ControlMode.PercentOutput, -75);
+    }
+    public void OutTake(){
+    	LeftIntake.set(ControlMode.PercentOutput, -75);
+    	RightIntake.set(ControlMode.PercentOutput, 75);
+    }
+    public void DropHand(){
+    	HandRotate.set(ControlMode.PercentOutput, 50);
+    }
+    public void RaiseHand(){
+    	HandRotate.set(ControlMode.PercentOutput, -50);
     }
 }
 
